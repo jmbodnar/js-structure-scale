ccbb6a5cda86bcb5d57128746952e583dfa7bb3c
Jonathan Bodnar <jon.bodnar@gmail.com>
2022-01-12 12:53:38 -0500

    Add project 2

---
+++ week01-wednesday-b.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+const Weight = (function () {
+  function Constructor(weight) {
+    this.weight = weight;
+  }
+
+  Constructor.prototype.inKilograms = function () {
+    return this.weight / 1000;
+  };
+
+  Constructor.prototype.inMilligrams = function () {
+    return this.weight * 1000;
+  };
+
+  return Constructor;
+})();
+
+const oneGram = new Weight(1);
+const kg = oneGram.inKilograms();
+const mg = oneGram.inMilligrams();
+
+console.log(
+  `${oneGram.weight} gram is one thousandth (${kg}) of a kilogram and ${mg} milligrams.`
+);
