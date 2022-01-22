a055b9ace997f334b21594f578a8e00fe61859a2
Jon Bodnar <jon.bodnar@gmail.com>
2022-01-22 06:56:25 -0500

    Add practice files

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,3 @@
 // @ts-check
-import * as fridayModule from "./week02/07-friday-a.js";
 
-const randoDato = new Date("April 11, 1975");
-const randoDatoWeekday = fridayModule.getWeekday(randoDato);
-
-console.log(randoDatoWeekday);
+import * as practice from "./week00/practice00.js";
