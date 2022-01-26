e333a829097ba5f960b0a5cb1bc25a2e6af59a77
Jonathan Bodnar <jon.bodnar@gmail.com>
2022-01-26 12:51:44 -0500

    Add settings object

--- www/js/main.js
+++ www/js/main.js
@@ -2,17 +2,25 @@
 
 import Time from "./week03/05-wednesday-project-a.js";
 
-const instanceA = new Time("April 11, 1975 10:30 AM");
-const instanceB = new Time("December 19, 1974 10:30 AM");
+const instanceA = new Time("April 11, 1975 10:30 AM", {
+  locale: "de",
+  dateStyle: "short",
+  timeStyle: "short",
+  month: "short",
+  weekday: "short",
+});
+const instanceB = new Time("April 11, 1975 10:30 AM");
 
-console.log(instanceA.dateObject);
-console.log(instanceA.getWeekday());
+console.log(instanceA.date);
+console.log(instanceA.getDay());
 console.log(instanceA.getMonth());
-console.log(instanceA.getDate());
+console.log(instanceA.getDay());
 console.log(instanceA.getTime());
+console.log(instanceA.getDate());
 
-console.log(instanceB.dateObject);
-console.log(instanceB.getWeekday());
+console.log(instanceB.date);
+console.log(instanceB.getDay());
 console.log(instanceB.getMonth());
-console.log(instanceB.getDate());
+console.log(instanceB.getDay());
 console.log(instanceB.getTime());
+console.log(instanceB.getDate());
--- www/js/week03/05-wednesday-project-a.js
+++ www/js/week03/05-wednesday-project-a.js
@@ -1,68 +1,96 @@
-// @ts-check
+/**
+ * Modify the date library to support developer options and immutable instances. Allow users to pass options to change
+ * the text returned from the getWeekday(), getMonth(), and related methods. Users can pass options to set the locales,
+ * dateStyle, timeStyle, weekday, and month values. They are set in the settings and allowed in the individual functions
+ * if desired.
+ *
+ * References:
+ * - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+ * - https://tc39.es/ecma402/#sec-intl-datetimeformat-constructor
+ * - https://www.w3schools.com/tags/ref_language_codes.asp
+ */
 
-function Constructor(date = []) {
+function Constructor(date = [], options = {}) {
   if (!Array.isArray(date)) {
     date = [date];
   }
 
-  this.dateObject = new Date(...date);
+  const settings = Object.assign(
+    {
+      locale: "en",
+      dateStyle: "long",
+      timeStyle: "long",
+      weekday: "long",
+      month: "long",
+    },
+    options
+  );
+
+  this.date = new Date(...date);
+
+  Object.freeze(settings);
+
+  Object.defineProperties(this, {
+    date: { value: this.date },
+    settings: { value: settings },
+  });
 }
 
 Constructor.prototype.date = function () {
-  return this.dateObject;
+  return this.date;
 };
 
-Constructor.prototype.getWeekday = function () {
-  return new Intl.DateTimeFormat("en", { weekday: "long" }).format(
-    this.dateObject
-  );
+Constructor.prototype.getDay = function () {
+  return new Intl.DateTimeFormat(this.settings.locale, {
+    weekday: this.settings.weekday,
+  }).format(this.date);
 };
 
 Constructor.prototype.getMonth = function () {
-  return new Intl.DateTimeFormat("en", { month: "long" }).format(
-    this.dateObject
-  );
+  return new Intl.DateTimeFormat(this.settings.locale, {
+    month: this.settings.month,
+  }).format(this.date);
 };
 
 Constructor.prototype.getTime = function () {
-  return new Intl.DateTimeFormat("en", { timeStyle: "long" }).format(
-    this.dateObject
-  );
+  return new Intl.DateTimeFormat(this.settings.locale, {
+    timeStyle: this.settings.timeStyle,
+  }).format(this.date);
 };
 
 Constructor.prototype.getDate = function () {
-  return new Intl.DateTimeFormat("en", { dateStyle: "long" }).format(
-    this.dateObject
-  );
+  return new Intl.DateTimeFormat(this.settings.locale, {
+    dateStyle: this.settings.dateStyle,
+  }).format(this.date);
 };
 
 Constructor.prototype.addSeconds = function (seconds) {
-  this.dateObject.setSeconds(this.dateObject.getSeconds() + seconds);
+  this.date.setSeconds(this.date.getSeconds() + seconds);
   return this;
 };
 
 Constructor.prototype.addMinutes = function (minutes) {
-  this.dateObject.setMinutes(this.dateObject.getMinutes() + minutes);
+  this.date.setMinutes(this.date.getMinutes() + minutes);
   return this;
 };
 
 Constructor.prototype.addHours = function (hours) {
-  this.dateObject.setHours(this.dateObject.getHours() + hours);
+  this.date.setHours(this.date.getHours() + hours);
   return this;
 };
 
 Constructor.prototype.addDays = function (days) {
-  this.dateObject.setDate(this.dateObject.getDate() + days);
+  this.date.setDate(this.date.getDate() + days);
   return this;
 };
 
 Constructor.prototype.addMonths = function (months) {
-  this.dateObject.setMonth(this.dateObject.getMonth() + months);
+  this.date.setMonth(this.date.getMonth() + months);
   return this;
 };
 
 Constructor.prototype.addYears = function (years) {
-  this.dateObject.setFullYear(this.dateObject.getFullYear() + years);
+  this.date.setFullYear(this.date.getFullYear() + years);
   return this;
 };
 
