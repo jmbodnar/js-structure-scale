e7e329bf53965550fe1981bf8cb86fdf9b7738bc
Jonathan Bodnar <jon.bodnar@gmail.com>
2022-01-26 15:35:36 -0500

    Finish wednesday project, week3

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,6 @@
-// @ts-check
-
 import Time from "./week03/05-wednesday-project-a.js";
 
-const instanceA = new Time("April 11, 1975 10:30 AM");
+const instanceA = new Time("April 11, 1918 10:30 AM");
 const instanceB = instanceA
   .addYears(5)
   .addYears(7)
@@ -16,5 +14,20 @@ const instanceB = instanceA
   .addSeconds(2)
   .addSeconds(34);
 
+const instanceC = new Time("December 19, 1975 10:42 AM", {
+  locale: "zh",
+  dateStyle: "short",
+  timeStyle: "short",
+  weekday: "short",
+  month: "long",
+});
+
 console.log(instanceA.date.toString());
+// Fri Apr 11 1975 10:30:00 GMT-0400 (Eastern Daylight Time) main.js:19:9
+
 console.log(instanceB.date.toString());
+// Tue Jul 14 1987 14:52:36 GMT-0400 (Eastern Daylight Time) main.js:20:9
+
+console.log(instanceC.getDate()); // 1975/12/19
+console.log(instanceC.getMonth()); // 十二月
+console.log(instanceC.getTime()); // 10:42
--- www/js/week03/05-wednesday-project-a.js
+++ www/js/week03/05-wednesday-project-a.js
@@ -7,7 +7,9 @@ function Constructor(date = [], options = {}) {
 
   const _settings = Object.assign(
     {
+      // https://www.w3schools.com/tags/ref_language_codes.asp
       locale: "en-US",
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
       dateStyle: "full",
       timeStyle: "full",
       weekday: "long",
@@ -119,3 +121,39 @@ Constructor.prototype.addYears = function (years) {
 };
 
 export default Constructor;
+
+/* // Imported and output in main.js
+import Time from "./week03/05-wednesday-project-a.js";
+
+const instanceA = new Time("April 11, 1918 10:30 AM");
+const instanceB = instanceA
+  .addYears(5)
+  .addYears(7)
+  .addMonths(2)
+  .addMonths(1)
+  .addDays(2)
+  .addDays(1)
+  .addHours(4)
+  .addMinutes(20)
+  .addMinutes(2)
+  .addSeconds(2)
+  .addSeconds(34);
+
+const instanceC = new Time("December 19, 1975 10:42 AM", {
+  locale: "zh",
+  dateStyle: "short",
+  timeStyle: "short",
+  weekday: "short",
+  month: "long",
+});
+
+console.log(instanceA.date.toString());
+// Fri Apr 11 1975 10:30:00 GMT-0400 (Eastern Daylight Time) main.js:19:9
+
+console.log(instanceB.date.toString());
+// Tue Jul 14 1987 14:52:36 GMT-0400 (Eastern Daylight Time) main.js:20:9
+
+console.log(instanceC.getDate()); // 1975/12/19
+console.log(instanceC.getMonth()); // 十二月
+console.log(instanceC.getTime()); // 10:42
+*/
