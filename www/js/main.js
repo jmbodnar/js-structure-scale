2bff9e40cb5e35062e25a6c36d089d502c9f24f0
Jon Bodnar <jon.bodnar@gmail.com>
2022-01-30 07:49:44 -0500

    Update project with stuff

--- www/js/main.js
+++ www/js/main.js
@@ -1,30 +1,29 @@
-import Time from "./week03/08-saturday-project-b.js";
+import Time from "./week03/08-friday-project-b.js";
 
-document.addEventListener("time:update", function (event) {
-  if (event.detail.afterUpdate.getFullYear() >= 2023) {
-    console.log(
-      "Event canceled: \n - ",
-      event.detail.beforeUpdate.toString(),
-      "(before)",
-      "\n - ",
-      event.detail.afterUpdate.toString(),
-      "(after)"
-    );
-    return event.preventDefault();
-  }
-  console.log("updatedTime: ", event.detail.afterUpdate.toString());
+document.addEventListener("time:ready", (event) => {
+  // console.log("time:ready:", event.detail.time.getDateString());
 });
 
-let startTime = new Time("January 1, 2021");
+document.addEventListener("time:update", (event) => {
+  console.log("time:update", event.detail.time.getDate());
+  if (event.detail.time.date.getFullYear() >= 2024) event.preventDefault();
+});
 
-let updatedTime = startTime
-  .addSeconds(1)
-  .addSeconds(1)
-  .addMinutes(20)
-  .addHours(10)
+let startTime = new Time("January 1, 2021", { locale: "de" });
+console.log("START TIME:", startTime.getDate());
+
+let endTime = startTime
+  .addSeconds(2)
+  .addSeconds(3)
+  .addMinutes(2)
+  .addMinutes(3)
+  .addHours(2)
+  .addHours(3)
+  .addDays(2)
   .addDays(3)
+  .addMonths(2)
   .addMonths(3)
-  .addYears(1)
-  .addMonths(12);
+  .addYears(2)
+  .addYears(3);
 
-console.log(updatedTime.getDateString());
+console.log("END TIME:", endTime.getDate());
