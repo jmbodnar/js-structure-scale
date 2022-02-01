ccd6b92f11f686ecbe64c7e9bb4632998edaaf01
Jon Bodnar <jon.bodnar@gmail.com>
2022-02-01 06:23:23 -0500

    Add stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,22 @@
+// For minification
+import { terser } from "rollup-plugin-terser";
+// Look into buble, too.
+
 const rollupConfig = [
   {
     input: "www/js/main.js",
     output: {
       file: "www/js/script.js",
       format: "iife",
+    },
+  },
+  {
+    input: "www/js/main.js",
+    output: {
+      file: "www/js/script.min.js",
+      format: "iife",
+      name: "version",
+      plugins: [terser()],
     },
   },
   {
@@ -17,14 +30,3 @@ const rollupConfig = [
 ];
 
 export default rollupConfig;
-
-// const rollupConfig = {
-//   input: "www/js/week03/07-friday-project-a.js",
-//   output: {
-//     file: "www/js/time.js",
-//     format: "iife",
-//     name: "Time",
-//   },
-// };
-
-// export default rollupConfig;
